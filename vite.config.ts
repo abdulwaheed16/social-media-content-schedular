fb15c0b84adb91fc3f1c27109960c5b4a909de96
abdulwaheed16 <abwaheed.ahmed@gmail.com>
2025-10-31 03:44:51 +0500

    added deployment settings

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base:"/social-media-content-schedular/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
